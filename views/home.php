26d2ec850f27111c6222c4783efc85631c9837d1
angieVentura <angiev.d.23@gmail.com>
2023-10-25 09:56:55 -0300

    Buscador

---
+++ views/home.php
@@ -0,0 +1,88 @@
+<div class="encabezado-home">
+    <div class="encabezado-cont contenedor">
+        <div class="cont-title">
+            <h1 class="title">Encontrá tu próximo alojamiento</h1>
+            <h3 class="sub title">Buscá tu habitación ideal y mucho más</h3>
+        </div>
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" aria-label="Text input with dropdown button">
+            <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                aria-expanded="false">Dropdown</button>
+            <ul class="dropdown-menu dropdown-menu-end">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="#">Separated link</a></li>
+            </ul>
+            <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                aria-expanded="false">Dropdown</button>
+            <select name="" id="" >
+                <option value="">lol</option>
+                <option value="">po</option>
+            </select>
+            <section class="ftco-section dropdown-menu dropdown-menu-end">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-6 text-center mb-5">
+                            <h2 class="heading-section">Calendar #03</h2>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="calendar-section">
+                                <div class="row no-gutters">
+                                    <div class="col-md-6">
+
+                                        <div class="calendar calendar-first" id="calendar_first">
+                                            <div class="calendar_header">
+                                                <button class="switch-month switch-left">
+                                                    <i class="fa fa-chevron-left"></i>
+                                                </button>
+                                                <h2></h2>
+                                                <button class="switch-month switch-right">
+                                                    <i class="fa fa-chevron-right"></i>
+                                                </button>
+                                            </div>
+                                            <div class="calendar_weekdays"></div>
+                                            <div class="calendar_content"></div>
+                                        </div>
+
+                                    </div>
+                                    <div class="col-md-6">
+
+                                        <div class="calendar calendar-second" id="calendar_second">
+                                            <div class="calendar_header">
+                                                <button class="switch-month switch-left">
+                                                    <i class="fa fa-chevron-left"></i>
+                                                </button>
+                                                <h2></h2>
+                                                <button class="switch-month switch-right">
+                                                    <i class="fa fa-chevron-right"></i>
+                                                </button>
+                                            </div>
+                                            <div class="calendar_weekdays"></div>
+                                            <div class="calendar_content"></div>
+                                        </div>
+
+                                    </div>
+
+                                </div> <!-- End Row -->
+
+                            </div> <!-- End Calendar -->
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+
+
+
+</div>
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
