5ea7c3ada55b85a01173fc5386a67accb1cc48c2
rojastamaraa <rojastamaranya@gmail.com>
2023-10-30 17:05:48 -0300

    front de login y registro

--- views/layout.php
+++ views/layout.php
@@ -4,8 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Hotel</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="scss/style.scss">
@@ -35,17 +34,20 @@
     <nav class="navbar navbar-expand-lg navbar-dark " aria-label="Fifth navbar example">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Hotel</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample05"
-                aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarsExample05">
-                <button class="btn-header">
-                    Registrarse
-                </button>
-                <button class="btn-header">
-                    Inicio Sesión
-                </button>
+                <a href="register.php">
+                    <button class="btn-header">
+                        Registrarse
+                    </button>
+                </a>
+                <a href="login.php">
+                    <button class="btn-header">
+                        Inicio Sesión
+                    </button>
+                </a>
             </div>
         </div>
     </nav>
@@ -73,12 +75,8 @@
     require_once $view . ".php";
     ?>
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
-        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
-        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- views/login.php
+++ views/login.php
@@ -1,1 +1,14 @@
-login épico
+<div class="container form">
+    <form action="../login.php">
+        <h2>Inicia sesión</h1>
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Correo electronico" name="Correo">
+        </div>
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Contrase;a" name="Pass">
+        </div>
+        <input class="btn btn-log" type="submit" value="Inicia sesión">
+        <br>
+        <a href="#">¿Olvidaste tu contraseña?</a> o <a href="register.php">¿No tenes una cuenta?</a>
+    </form>
+</div>
