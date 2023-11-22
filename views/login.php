08756f194c6ce6c0564e9273c3cd08387aa50f69
rojastamaraa <rojastamaranya@gmail.com>
2023-11-22 17:05:21 -0300

    +footer

--- views/login.php
+++ views/login.php
@@ -1,14 +1,17 @@
-<div class="container form">
+<div class="container formGRANDE">
     <form action="../login.php">
-        <h2>Inicia sesión</h1>
-        <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Correo electronico" name="Correo">
+        <div class="formm">
+            <h2>Inicia sesión</h1>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Correo electronico" name="Correo">
+                </div>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Contrase;a" name="Pass">
+                </div>
+                <input class="btn btn-log" type="submit" value="Inicia sesión">
+                <br>
+                <a href="#">¿Olvidaste tu contraseña?</a> o <a href="register.php">¿No tenes una cuenta?</a>
         </div>
-        <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Contrase;a" name="Pass">
-        </div>
-        <input class="btn btn-log" type="submit" value="Inicia sesión">
-        <br>
-        <a href="#">¿Olvidaste tu contraseña?</a> o <a href="register.php">¿No tenes una cuenta?</a>
+
     </form>
 </div>
