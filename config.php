8c6a56257adbdea811c68552c32be053220ebe50
rojastamaraa <rojastamaranya@gmail.com>
2023-11-23 20:24:35 -0300

    tabla usuarios y paginador

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 try {
     // Configuración de la conexión PDO
-    $dsn = "sqlsrv:Server=ANGIE\SQLEXPRESS;Database=tpBD";
+    $dsn = "sqlsrv:Server=TAMARA\SQLEXPRESS02;Database=tpBD";
     $usuario = "";
     $contrasena = "root";
-
+    define('CANT_REG_PAG', 2);
     // Crear la conexión PDO
     $conn = new PDO($dsn, $usuario, $contrasena);
 
---
+++ tablaUsu.php
@@ -0,0 +1,33 @@
+<?php
+require_once("config.php");
+$view = "tablaUsu";
+
+$query = "SELECT Nombre, Email from Clientes";
+$result = $conn->query($query);
+
+if (!$result) {
+    throw new Exception("Error en la consulta: " . implode(", ", $conn->errorInfo()));
+}
+
+$usuarios = $result->fetchAll(PDO::FETCH_ASSOC);
+$numRows = 0;
+foreach($usuarios as $usu){
+    $numRows=$numRows+1;
+}
+
+$pag = (isset($_GET['pag']) ? $_GET['pag'] : 1);
+
+$desde = CANT_REG_PAG * ($pag - 1);
+
+$por_pag = CANT_REG_PAG;
+
+$query2 = "SELECT Nombre, Email FROM (SELECT Nombre, Email, ROW_NUMBER() OVER (ORDER BY Nombre) AS RowNum FROM Clientes) AS SubQuery WHERE RowNum BETWEEN $desde + 1 AND $desde + $por_pag";
+$result2 = $conn->query($query2);
+if(!$result2){
+    throw new Exception("Error en la consulta: " . implode(", ", $conn->errorInfo()));
+}
+
+$usuarios2 = $result2->fetchAll(PDO::FETCH_ASSOC);
+$cant_pag = ceil($numRows/CANT_REG_PAG);
+
+require_once("views/layout.php");
