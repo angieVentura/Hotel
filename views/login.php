5d6ce4325a8f8afa5b2ca20883a24f2ebc8ca4ed
rojastamaraa <rojastamaranya@gmail.com>
2023-11-23 15:06:49 -0300

    pen

--- views/login.php
+++ views/login.php
@@ -6,7 +6,7 @@
                     <input type="text" class="form-control" placeholder="Correo electronico" name="Correo">
                 </div>
                 <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Contrase;a" name="Pass">
+                    <input type="text" class="form-control" placeholder="Contraseña" name="Pass">
                 </div>
                 <input class="btn btn-log" type="submit" value="Inicia sesión">
                 <br>
